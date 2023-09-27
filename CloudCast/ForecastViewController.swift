4d5f856ba15c0874cb5722b9169bd3b65b4d8eaf
Macee Qi <maceeqi@gmail.com>
2023-09-26 22:53:42 -0400

    add mock data

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -18,7 +18,17 @@ class ForecastViewController: UIViewController {
     // Fired when the view has finished loading for the first time
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fakeData = WeatherForecast(temperature: 25.0, date: Date(), weatherCode: .partlyCloudy)
+        configure(with: fakeData)
     }
 
-
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
+    }
 }
