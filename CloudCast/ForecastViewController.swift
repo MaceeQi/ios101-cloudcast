73b14f798c4296a2c3976158ad0b67fe17ed52e2
Macee Qi <maceeqi@gmail.com>
2023-09-26 22:40:35 -0400

    link UI elements to code

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     // Fired when the view has finished loading for the first time
     override func viewDidLoad() {
         super.viewDidLoad()
