7dd8b0b41d5e58d8dfd3a481955dec8613c54659
Macee Qi <maceeqi@gmail.com>
2023-09-26 21:22:42 -0400

    create initial view controller

---
+++ CloudCast/ForecastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Macee Qi on 9/26/23.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    // Fired when the view has finished loading for the first time
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
